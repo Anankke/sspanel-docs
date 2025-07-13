499f31a829ad201b7a0ef530dc775fad8df39eda
Anankke <anankke@pm.me>
2025-07-13 09:27:19 +0000

    config: update site configuration and integrations

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,6 +92,11 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true, // 禁用主题切换开关
+        respectPrefersColorScheme: false,
+      },
       navbar: {
         title: 'SSPanel-Docs',
         logo: {
